eb321129d94ffd8db4a3eece1865de5990ca6365
deesebc <sinliston@gmail.com>
2013-04-17 12:53:16 +0200

    - reorganizamos el metodo para permitir la busqueda filtrada

--- discoteca/src/main/java/es/discoteca/app/json/SearchDiscController.java
+++ discoteca/src/main/java/es/discoteca/app/json/SearchDiscController.java
@@ -3,11 +3,18 @@
  */
 package es.discoteca.app.json;
 
+import java.io.IOException;
 import java.io.Serializable;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.JsonProcessingException;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -32,19 +39,52 @@ public class SearchDiscController implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	private static final Logger LOGGER = Logger.getLogger(SearchDiscController.class);
+
 	@Autowired
 	private DiscoService service;
 
 	@Autowired
 	private PaginatorUtil<Disco, DiscoJson> utility;
 
+	private Page<Disco> getDisco(final String source, final Pageable pageable)
+			throws JsonProcessingException, IOException {
+		Page<Disco> exit = null;
+		if (StringUtils.isBlank(source)) {
+			exit = service.findSearchDiscs(null, null, pageable);
+		} else {
+			ObjectMapper mapper = new ObjectMapper();
+			JsonNode jsonFilter = mapper.readTree(source);
+			List<JsonNode> rules = jsonFilter.findValues("rules");
+			String field, data, grupo = "", nombre = "";
+			for (JsonNode node : rules) {
+				field = node.findValues("field").get(0).asText();
+				data = node.findValues("data").get(0).asText();
+				if ("nombre".equals(field)) {
+					nombre = data;
+				} else if ("grupo".equals(field)) {
+					grupo = data;
+				}
+			}
+
+			exit = service.findSearchDiscs(nombre, grupo, pageable);
+
+		}
+		return exit;
+	}
+
 	@RequestMapping(value = "/jsonSearchDisc.htm", produces = "application/json")
 	public @ResponseBody
 	JqGridResponse<DiscoJson> records(@RequestBody final JqGridRequest jqGridRequest,
 			final HttpServletRequest request, final HttpServletResponse response) {
 
-		Pageable pageable = utility.getPageable(jqGridRequest);
-		Page<Disco> page = service.findAll(pageable);
+		Page<Disco> page = null;
+		try {
+			Pageable pageable = utility.getPageable(jqGridRequest);
+			page = getDisco(jqGridRequest.getFilters(), pageable);
+		} catch (Exception except) {
+			LOGGER.error("Error en la busqueda: ", except);
+		}
 		return utility.getBookJqGridRes(page, DiscoJson.class);
 	}
 }
