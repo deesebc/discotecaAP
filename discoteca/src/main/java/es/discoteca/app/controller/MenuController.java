a7c80020e106527234cc40985c5173d3c1a36739
deesebc <daniel@daniel-HP-G62-PC>
2014-05-25 09:37:18 +0200

    Añadimos los path para el inicio y la pagina de denegar acceso

--- discoteca/src/main/java/es/discoteca/app/controller/MenuController.java
+++ discoteca/src/main/java/es/discoteca/app/controller/MenuController.java
@@ -4,6 +4,7 @@
 package es.discoteca.app.controller;
 
 import java.io.IOException;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -11,7 +12,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -23,53 +23,47 @@ import org.springframework.web.servlet.ModelAndView;
 public class MenuController {
 	protected final Logger logger = Logger.getLogger(getClass());
 
-	@ExceptionHandler(ArithmeticException.class)
-	public ModelAndView handleAException(final ArithmeticException ex) {
+	@RequestMapping(value = "/accessdenied.htm")
+	public ModelAndView accessDenied(final HttpServletRequest request,
+			final HttpServletResponse response) throws ServletException,
+			IOException {
+		logger.info("accessDenied - init");
+		return new ModelAndView("access.denied.page");
 
-		logger.info("handleAException - Catching: " + ex.getClass().getSimpleName());
-		ModelAndView modelAndView = new ModelAndView("error.page");
-		// modelAndView.setViewName("error.page");
-		modelAndView.addObject("name", ex.getClass().getSimpleName());
-		modelAndView.addObject("user", "Dani");
-
-		return modelAndView;
 	}
 
-	@ExceptionHandler(IOException.class)
-	public ModelAndView handleIOException(final IOException ex) {
+	@RequestMapping(value = "/hello.htm")
+	public ModelAndView hello(final HttpServletRequest request,
+			final HttpServletResponse response) throws ServletException,
+			IOException {
+		logger.info("hello - init");
+		String now = (new Date()).toString();
+		logger.info("Returning hello view with " + now);
 
-		logger.info("handleIOException - Catching: " + ex.getClass().getSimpleName());
-		ModelAndView modelAndView = new ModelAndView("error.page");
-		// modelAndView.setViewName("error.page");
-		modelAndView.addObject("name", ex.getClass().getSimpleName());
-		modelAndView.addObject("user", "Dani");
-
-		return modelAndView;
-	}
-
-	@RequestMapping(value = "/search/disc.htm")
-	public ModelAndView searchDisc(final HttpServletRequest request,
-			final HttpServletResponse response) throws ServletException, IOException {
-
-		logger.info("Pasamos por MenuController");
-
-		return new ModelAndView("search.disc.page");
+		return new ModelAndView("hello.page", "now", now);
 
 	}
 
 	@RequestMapping(value = "/search/book.htm")
-	public ModelAndView searchInterprent(final HttpServletRequest request,
-			final HttpServletResponse response) throws ServletException, IOException,
-			ArithmeticException {
-
-		logger.info("Pasamos por MenuController");
+	public ModelAndView searchBook(final HttpServletRequest request,
+			final HttpServletResponse response) throws ServletException,
+			IOException, ArithmeticException {
+		logger.info("searchBook - init");
 		// boolean throwException = true;
-
 		// if (throwException) {
-		// // throw new IOException("This is my IOException");
+		// throw new IOException("This is my IOException");
 		// logger.info(1 / 0);
 		// }
 		return new ModelAndView("search.book.page");
+
+	}
+
+	@RequestMapping(value = "/search/disc.htm")
+	public ModelAndView searchDisc(final HttpServletRequest request,
+			final HttpServletResponse response) throws ServletException,
+			IOException {
+		logger.info("searchDisc - init");
+		return new ModelAndView("search.disc.page");
 
 	}
 }
